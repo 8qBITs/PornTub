9c2b5fc714244ec4f0f36952445c43390f8c66ff
8qBIT <mrdado243@gmail.com>
2023-07-06 23:58:00 +0200

    Updated script
    Added video previews, 5 second clips.
    Added background library indexing
    Added video name filter that will remove any special characters as well words listed inside the config.json.

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.set('view engine', 'ejs');
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/thumbnails', express.static(path.join(__dirname, 'thumbnails')));
+app.use('/clips', express.static(path.join(__dirname, 'clips')));
 app.use('/static', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
 
@@ -35,7 +36,8 @@ app.get('/', (req, res) => {
     const videoInfo = videosData.map(video => ({
         uniqueId: video.uniqueId,
         thumbnail: video.thumbnailPath,
-        name: video.videoName
+        name: video.videoName,
+        //clip: video.clipPath
     }));
 
     res.render('index', { videoInfo });
@@ -79,11 +81,11 @@ app.get('/playVideo/:id', (req, res) => {
 
 
 // Route to serve video files in chunks
-app.get('/video/:uniqueId', (req, res) => {
+app.get('/video/:uniqueId', async (req, res) => {
     // Load videos.json
     let videosData;
     try {
-        videosData = JSON.parse(fs.readFileSync('videos.json'));
+        videosData = JSON.parse(await fs.promises.readFile('videos.json', 'utf-8'));
     } catch (err) {
         console.error('Error reading videos.json:', err);
         return res.status(500).send('Server error');
@@ -96,7 +98,15 @@ app.get('/video/:uniqueId', (req, res) => {
     }
 
     const videoPath = path.join(__dirname, 'videos', videoInfo.videoFile);
-    const stat = fs.statSync(videoPath);
+
+    let stat;
+    try {
+        stat = await fs.promises.stat(videoPath);
+    } catch (err) {
+        console.error('Error stating video file:', err);
+        return res.status(500).send('Server error');
+    }
+    
     const fileSize = stat.size;
     const range = req.headers.range;
 
@@ -131,8 +141,6 @@ app.get('/video/:uniqueId', (req, res) => {
         fs.createReadStream(videoPath).pipe(res);
     }
 });
-
-
 
 app.get('/getThumbnails', (req, res) => {
     const videoDir = path.join(__dirname, 'videos');
@@ -169,15 +177,33 @@ app.get('/getThumbnails', (req, res) => {
 });
 
 app.get('/scanLibrary', async (req, res) => {
+    // Check if the IP address of the request is 77.38.24.76
+    /*
+    const ip = req.ip || req.connection.remoteAddress;
+    if (ip !== '77.38.24.76') {
+        return res.status(403).send('Access denied');
+    }*/
+    
+    // Send response immediately
+    res.send('Library scanning has started.');
+
     try {
         // Directories
         const videoDir = path.join(__dirname, 'videos');
         const thumbnailDir = path.join(__dirname, 'thumbnails');
+        const clipsDir = path.join(__dirname, 'clips');
 
-        // Create thumbnails directory if it doesn't exist
+        // Create thumbnails and clips directory if they don't exist
         if (!fs.existsSync(thumbnailDir)){
             fs.mkdirSync(thumbnailDir);
         }
+        if (!fs.existsSync(clipsDir)){
+            fs.mkdirSync(clipsDir);
+        }
+
+        // Load config.json
+        const config = JSON.parse(fs.readFileSync('config.json'));
+        const removedWords = config.removedWords || [];
 
         // Scan videos directory
         const files = fs.readdirSync(videoDir);
@@ -195,22 +221,33 @@ app.get('/scanLibrary', async (req, res) => {
                 });
             });
 
-            // Generate thumbnail
+            // Generate thumbnail in the background
             const thumbnailName = videoFile + '.jpg';
             const thumbnailPath = path.join(thumbnailDir, thumbnailName);
-            await new Promise(resolve => {
-                ffmpeg(videoPath)
-                    .screenshot({
-                        timestamps: ['5%'],
-                        filename: path.basename(thumbnailPath),
-                        folder: path.dirname(thumbnailPath)
-                    })
-                    .on('end', resolve);
-            });
+            ffmpeg(videoPath)
+                .screenshot({
+                    timestamps: ['5%'],
+                    filename: path.basename(thumbnailPath),
+                    folder: path.dirname(thumbnailPath)
+                });
+
+            // Generate 5-second clip at 10% in the background
+            const clipStartTime = videoLength * 0.10; // Start time at 10% of video length
+            const clipName = videoFile + '_clip.mp4';
+            const clipPath = path.join(clipsDir, clipName);
+            ffmpeg(videoPath)
+                .seekInput(clipStartTime)
+                .duration(5) // 5-second clip
+                .noAudio() // Exclude audio
+                .output(clipPath)
+                .run();
 
             // Extract and clean video name
             let videoName = path.parse(videoFile).name;
-            videoName = videoName.replace(/[.-]/g, ' ').replace(/\b\d{3,}\b/g, '');
+            videoName = videoName.replace(/[^\w\s]|[\d]|_/g, " ").replace(/\s+/g, " "); // Remove special characters, dots and digits
+            removedWords.forEach(word => {
+                videoName = videoName.replace(new RegExp('\\b' + word + '\\b', 'gi'), ''); // Remove words in config
+            });
 
             // Generate a unique ID for the video
             const uniqueId = uuidv4();
@@ -221,21 +258,70 @@ app.get('/scanLibrary', async (req, res) => {
                 videoName,
                 videoFile,
                 videoLength,
-                thumbnailPath: `/thumbnails/${thumbnailName}` // Save relative path
+                thumbnailPath: `/thumbnails/${thumbnailName}`, // Save relative path
+                clipPath: `/clips/${clipName}` // Save relative path for clip
             });
         }
 
-        // Save video information to videos.json
+        // Save video information to videos.json once processing is complete
         fs.writeFileSync('videos.json', JSON.stringify(videoInfo, null, 2));
 
-        res.send('Library scanned, thumbnails generated and information saved to videos.json.');
+        console.log('Library scanned, thumbnails generated, clips created, and information saved to videos.json.');
     } catch (err) {
         console.error(err);
-        res.status(500).send('An error occurred.');
+    }
+});
+
+app.get('/playClip/:uniqueId', async (req, res) => {
+    try {
+        // Your asynchronous logic here...
+        
+        const uniqueId = req.params.uniqueId;
+
+        // Load videos.json to get the clip file path
+        const videosData = JSON.parse(await fs.promises.readFile('videos.json'));
+
+        // Find the video with the matching uniqueId
+        const videoInfo = videosData.find(video => video.uniqueId === req.params.uniqueId);
+        if (!videoInfo) {
+            return res.status(404).send('Clip not found');
+        }
+
+        const clipPath = path.join(__dirname, 'clips', path.basename(videoInfo.clipPath));
+        const stat = fs.statSync(clipPath);
+        const fileSize = stat.size;
+        const range = req.headers.range;
+
+        if (range) {
+            const parts = range.replace(/bytes=/, "").split("-");
+            const start = parseInt(parts[0], 10);
+            const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+            const chunksize = (end - start) + 1;
+            const file = fs.createReadStream(clipPath, { start, end });
+            const head = {
+                'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                'Accept-Ranges': 'bytes',
+                'Content-Length': chunksize,
+                'Content-Type': 'video/mp4',
+            };
+            res.writeHead(206, head);
+            file.pipe(res);
+        } else {
+            const head = {
+                'Content-Length': fileSize,
+                'Content-Type': 'video/mp4',
+            };
+            res.writeHead(200, head);
+            fs.createReadStream(clipPath).pipe(res);
+        }
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
     }
 });
 
 
 // Start the Express.js server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 6969;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
